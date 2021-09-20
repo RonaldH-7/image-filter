74bee1eb277fe7cff9780647b4c03936eda63653
RonaldH-7 <ronald.hua7@gmail.com>
2021-09-20 13:21:41 -0400

    Added ability to upload images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,13 @@ import * as data from '../assets/filters.json';
 export class AppComponent {
   filters: any = data;
   imagePath: string = "../assets/sunflower.jpg";
+
+  onUpload(event: any, reference: any) {
+    let fileReader: FileReader = new FileReader();
+    fileReader.onloadend = (e: any) => {
+      this.imagePath = e.target.result;
+    }
+    fileReader.readAsDataURL(event.files[0]);
+    reference.clear();
+  }
 }
